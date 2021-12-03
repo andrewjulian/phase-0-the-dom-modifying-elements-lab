06813cf45448a2694ab80c8778182e60b0c3b163
+ <+>
2021-12-02 17:49:18 -0700

    Completed Assignment Creating and Inserting DOM Node Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.h1 = "victory"
+newHeader.id = "victory";
+newHeader.textContent = "ANDREW is the champion";
+document.body.append(newHeader);
